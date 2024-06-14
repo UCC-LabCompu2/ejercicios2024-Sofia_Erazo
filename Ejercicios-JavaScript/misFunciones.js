8807235e98517b1f05344755cba8d94a4f5c0649
Sofia Erazo <2318441@ucc.edu.ar>
2024-06-14 00:30:21 -0300

    se realizo el ejercicio de conversion de grados y radianes

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -31,3 +31,16 @@ function cambiarunidades(id, value) {
     }
 
 }
+
+function convertir(id) {
+    var grad, rad;
+    if (id == "grados") {
+        grad = document.getElementById("grados").value;
+        rad = (grad * Math.PI)/180;
+    } else if (id == "radianes") {
+        rad = document.getElementById("radianes").value;
+        grad = (rad * 180) / Math.PI;
+    }
+    document.getElementById("grados").value = grad;
+    document.getElementById("radianes").value = rad;
+    }
